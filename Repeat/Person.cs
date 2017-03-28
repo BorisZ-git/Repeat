f1eb6b46d14297cd2bf2199454ba784f8dedf824
BorisZ-git <ribos@bk.ru>
2017-03-29 00:23:49 +0300

    start repeat level1

---
+++ Repeat/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repeat
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Hight { get; set; }
+        public string Weight { get; set; }
+    }
+}
---
+++ Repeat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Boris Z
+1. Написать программу “Анкета”. Последовательно задаются вопросы (имя, фамилия, возраст,
+рост, вес). В результате вся информация выводится в одну строчку.
+а) используя  склеивание;
+б) используя форматированный вывод.
+1. Ввести вес и рост человека. Расчитать и вывести индекс массы тела(ИМТ) по формуле
+I=m/(h*h); где m­масса тела в килограммах, h ­ рост в метрах*/
+
+namespace Repeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person db = new Person();
+            
+        }
+    }
+}
