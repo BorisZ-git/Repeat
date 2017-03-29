d97cca958ccf1dc47de074cee356d3edb06b4bc2
BorisZ-git <ribos@bk.ru>
2017-03-30 00:33:09 +0300

    wrote class Anket that contained class Person with property like Name Last Name Age and etc\\ wrote method Show this property in one line

---
+++ Repeat/Anket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repeat
+{
+    class Anket
+    {
+        public static Person Interview()
+        {
+            Person copy = new Person();
+            copy.FName = Console.ReadLine();
+            copy.LName = Console.ReadLine();
+            copy.Age = Console.ReadLine();
+            copy.Hight = Console.ReadLine();
+            copy.Weight = Console.ReadLine();
+            return copy;
+        }
+        public static void ToString(Person copy)
+        {
+            Console.WriteLine($"{copy.FName} {copy.LName} {copy.Age} {copy.Hight} {copy.Weight}");
+        }
+    }
+    class Person
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Age { get; set; }
+        public string Hight { get; set; }
+        public string Weight { get; set; }
+    }
+}
