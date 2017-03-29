ae54ef64622200b4d50314e512941149ed1edb43
BorisZ-git <ribos@bk.ru>
2017-03-30 00:46:57 +0300

    start Task2 \\ Wrote class Distance contained variables of two points x and y \\ created constructor for set this variables

---
+++ Repeat/Distance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repeat
+{
+    class Distance
+    {
+        int x1,x2;
+        int y1,y2;
+        public Distance()
+        {
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
