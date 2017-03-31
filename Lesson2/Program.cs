e9957212b0a646a01037787a533c7d585c4f0c9a
BorisZ-git <ribos@bk.ru>
2017-03-31 17:58:56 +0300

    completed Task 4

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -76,6 +76,10 @@ namespace Lesson2
 
             #endregion *** Task 3***
             #region *** Task 4***
+
+            Personal user = new Personal("log", "pass");
+            user.Identification();
+
             #endregion *** Task 4***
             #region *** Task 5***
             #endregion *** Task 5***
