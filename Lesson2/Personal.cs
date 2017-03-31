b111b5c4414cbbd107c735332f75e363a787c5ca
BorisZ-git <ribos@bk.ru>
2017-03-31 17:22:27 +0300

    add class Personal where wrote constructor describe login and password of User

---
+++ Lesson2/Personal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Personal
+    {
+        string login, password;
+        public Personal(string login,string password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+    }
+}
