b3249fcc7999e02a0a7bc5feb710aebe166a73a2
BorisZ-git <ribos@bk.ru>
2017-03-31 17:52:14 +0300

    correct method Check in class Personal \\ now he get one string and compare it with login and password if them equal he return true \\ wrote method Identification which circle use method Check.

--- Lesson2/Personal.cs
+++ Lesson2/Personal.cs
@@ -14,10 +14,40 @@ namespace Lesson2
             this.login = login;
             this.password = password;
         }
-        public bool Identification(string login, string password)
+        public void Identification()
         {
-            if (this.login == login) return true;
-            else if (this.password == password) return true;
+            int count = 3;
+            string log = null;
+            string pass = null;
+            while (count!=0)
+            {
+                while (true)
+                {
+                    if (Check(log)) break;
+                    if (count == 0) break;
+                    else
+                    {
+                        log = Console.ReadLine();
+                        if (Check(log)) break;
+                        else count--;
+                    }
+                }
+                if (count == 0) break;
+                pass = Console.ReadLine();
+                if (!Check(pass))
+                {
+                    count--;
+                    continue;
+                }
+                break;
+            }
+            if (count == 0) Console.WriteLine($"count = {count}");
+            Console.WriteLine("You Pass!");
+        }
+        public bool Check(string str)
+        {
+            if (this.login == str) return true;
+            else if (this.password == str) return true;
             else return false;
         }
     }
